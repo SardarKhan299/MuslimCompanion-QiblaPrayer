346598a54b1a47177aec2abc134034720d5cd120
SardarkhanStella <skhan@stellatechnology.com>
2023-09-02 01:44:19 +0500

    Add splash screen and its resources.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/SplashActivity.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/SplashActivity.kt
@@ -2,10 +2,21 @@ package com.qibla.qiblacompass.prayertime.finddirection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makeFullScreen()
         setContentView(R.layout.activity_splash)
+    }
+    private fun makeFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide()
     }
 }
