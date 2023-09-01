02608d9fe39a60c6595f46d3e5d2caa38d1b1393
SardarkhanStella <skhan@stellatechnology.com>
2023-09-02 01:59:50 +0500

    Add fonts and gradle files.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/SplashActivity.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/SplashActivity.kt
@@ -1,15 +1,25 @@
 package com.qibla.qiblacompass.prayertime.finddirection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
+import com.qibla.qiblacompass.prayertime.finddirection.views.OnboardingActivity
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         makeFullScreen()
         setContentView(R.layout.activity_splash)
+        Timer("splash", false).schedule(3000) {
+            startActivity(Intent(this@SplashActivity, OnboardingActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
     private fun makeFullScreen() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
---
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/views/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.qibla.qiblacompass.prayertime.finddirection.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qibla.qiblacompass.prayertime.finddirection.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
