784cf08ff09d3195c0cd1c8766d84f0d0201a182
SardarkhanStella <skhan@stellatechnology.com>
2023-09-12 20:32:59 +0500

    Add essential DTO

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/data/remote/ApiInterface.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/data/remote/ApiInterface.kt
@@ -1,8 +1,9 @@
 package com.qibla.qiblacompass.prayertime.finddirection.data.remote
 
+import com.qibla.qiblacompass.prayertime.finddirection.data.remote.dto.ApiDto
 import retrofit2.http.GET
 
 interface ApiInterface {
     @GET("v1/api")
-    suspend fun getPrayerTimings()
+    suspend fun getPrayerTimings():List<ApiDto>
 }
