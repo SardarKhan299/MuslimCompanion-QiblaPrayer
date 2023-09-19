6e388a0cceb5e25f27ff2f091242790197ff8af6
amalik <amalik@stellatechnology.com>
2023-09-19 14:19:37 +0500

    Working on login screen.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/login/LoginFragment.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/login/LoginFragment.kt
@@ -1,26 +1,36 @@
 package com.qibla.qiblacompass.prayertime.finddirection.presentation.views.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import com.qibla.qiblacompass.prayertime.finddirection.R
+import com.qibla.qiblacompass.prayertime.finddirection.base.BaseFragment
+import com.qibla.qiblacompass.prayertime.finddirection.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment() {
-
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            loginFragment = this@LoginFragment
+        }
+
+        binding.btnLogin.setOnClickListener {
+            Log.d("LoginFragment" ,"onViewCreated:Login Button Clicked. ")
+         //   Navigation.findNavController(requireView()).navigate(R.id.signUpFragment)
+
+        }
     }
 
 }
