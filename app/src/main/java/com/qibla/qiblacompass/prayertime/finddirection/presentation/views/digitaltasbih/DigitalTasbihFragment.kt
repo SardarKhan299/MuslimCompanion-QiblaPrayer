43e10d9d765902cf1004fb18fcaea1f48db15155
amalik <amalik@stellatechnology.com>
2023-11-16 15:59:48 +0500

    Fixed some layout issues.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/digitaltasbih/DigitalTasbihFragment.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/digitaltasbih/DigitalTasbihFragment.kt
@@ -5,23 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.qibla.qiblacompass.prayertime.finddirection.R
+import com.qibla.qiblacompass.prayertime.finddirection.base.BaseFragment
+import com.qibla.qiblacompass.prayertime.finddirection.common.closeCurrentScreen
+import com.qibla.qiblacompass.prayertime.finddirection.common.hideActionBar
+import com.qibla.qiblacompass.prayertime.finddirection.databinding.FragmentDigitalTasbihBinding
 
 
-class DigitalTasbihFragment : Fragment() {
+class DigitalTasbihFragment :BaseFragment<FragmentDigitalTasbihBinding>(R.layout.fragment_digital_tasbih) {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        (activity as AppCompatActivity?)?.hideActionBar()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_digital_tasbih, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            digitalTasbihCounterFragment = this@DigitalTasbihFragment
+        }
+        val toolbar = binding.toolbarDigitalTasbih
+        val toolbarText = toolbar.titleCounter
+        toolbarText.text = "Tasbih Counter"
+        toolbar.groupToolbarTasbihCounter.visibility = View.VISIBLE
+        toolbar.imgNavigateBack.setOnClickListener {
+            findNavController().closeCurrentScreen()
+        }
     }
 
 }
