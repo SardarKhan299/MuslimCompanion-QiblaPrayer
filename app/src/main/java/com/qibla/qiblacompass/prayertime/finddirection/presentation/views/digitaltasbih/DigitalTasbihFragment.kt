b841d88e6a2de2e40af0e5cddee11ae40623b168
amalik <amalik@stellatechnology.com>
2023-11-16 15:33:24 +0500

    Design layout for  Digital tasbih and add fragment in navigation graph

---
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/digitaltasbih/DigitalTasbihFragment.kt
@@ -0,0 +1,27 @@
+package com.qibla.qiblacompass.prayertime.finddirection.presentation.views.digitaltasbih
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.qibla.qiblacompass.prayertime.finddirection.R
+
+
+class DigitalTasbihFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_digital_tasbih, container, false)
+    }
+
+}
