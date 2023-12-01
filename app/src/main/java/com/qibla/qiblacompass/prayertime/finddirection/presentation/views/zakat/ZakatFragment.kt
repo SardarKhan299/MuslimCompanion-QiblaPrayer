dfab27c9a9c43167e3b869568dc762769b473e9f
amalik <amalik@stellatechnology.com>
2023-12-01 15:01:48 +0500

    Add Adapter for zakat screen and fixed UI issues.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/zakat/ZakatFragment.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/zakat/ZakatFragment.kt
@@ -1,12 +1,11 @@
 package com.qibla.qiblacompass.prayertime.finddirection.presentation.views.zakat
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.qibla.qiblacompass.prayertime.finddirection.R
 import com.qibla.qiblacompass.prayertime.finddirection.base.BaseFragment
 import com.qibla.qiblacompass.prayertime.finddirection.common.closeCurrentScreen
@@ -15,6 +14,7 @@ import com.qibla.qiblacompass.prayertime.finddirection.databinding.FragmentZakat
 
 
 class ZakatFragment : BaseFragment<FragmentZakatBinding>(R.layout.fragment_zakat) {
+    lateinit var recyclerView: RecyclerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,9 +28,26 @@ class ZakatFragment : BaseFragment<FragmentZakatBinding>(R.layout.fragment_zakat
             zakatFragment = this@ZakatFragment
         }
         binding.toolbarZakat.groupToolbarSubScreenProfile.visibility = View.VISIBLE
-        binding.toolbarZakat.tvToolbarSubScreen.text = "Zakat"
+        binding.toolbarZakat.tvToolbarSubScreen.text = getString(R.string.zakat_calculator)
         binding.toolbarZakat.viewSubScreen.setOnClickListener {
             findNavController().closeCurrentScreen()
         }
+        recyclerView = binding.layoutZakatFragment.findViewById(R.id.recycler_view_zakat)
+        val gridLayoutManager =
+            GridLayoutManager(mContext, 3) // Adjust the number of columns as needed
+        recyclerView.layoutManager = gridLayoutManager
+
+        val data = ArrayList<ZakatData>()
+        data.add(ZakatData(R.drawable.ic_gold, getString(R.string.gold), getString(R.string._10_tola)))
+        data.add(ZakatData(R.drawable.ic_silver, getString(R.string.Silver), "5 Tola"))
+        data.add(ZakatData(R.drawable.ic_stones, getString(R.string.Stones), "0.9 million"))
+        data.add(ZakatData(R.drawable.ic_agriculture, getString(R.string.agriculture), "2.3 million"))
+        data.add(ZakatData(R.drawable.ic_property, getString(R.string.property), "1.5 million"))
+        data.add(ZakatData(R.drawable.ic_business, getString(R.string.business), "1.2 million"))
+        data.add(ZakatData(R.drawable.ic_partnership, getString(R.string.partnership), "1000 PKR"))
+        data.add(ZakatData(R.drawable.ic_loan, getString(R.string.loan), "5 million"))
+
+        val adapter = ZakatAdapter(data)
+        recyclerView.adapter = adapter
     }
 }
