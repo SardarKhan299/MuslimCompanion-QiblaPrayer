56f88b855868f45edc5fe8dbccd5cc88a6be9744
amalik <amalik@stellatechnology.com>
2023-12-29 12:00:42 +0500

    fixed guideline issues.

---
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/common/PopUpDialog.kt
@@ -0,0 +1,48 @@
+package com.qibla.qiblacompass.prayertime.finddirection.common
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.qibla.qiblacompass.prayertime.finddirection.R
+
+class PopUpDialog(var title : String,
+                  var message : String,
+                  val ok_btn_callback: (String) -> Unit,
+                  val error_icn: Int = 0
+                  ) : DialogFragment() {
+    override fun onStart() {
+        super.onStart()
+        Log.d("PopUpDialog"::class.simpleName, "onStart: ")
+        dialog?.setCancelable(false)
+        val window: Window = dialog?.window!!
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView: View = inflater.inflate(
+            R.layout.fragment_popup_dialog,
+            container,
+            false)
+        rootView.findViewById<TextView>(R.id.tv_dialog_title).text = title
+        rootView.findViewById<TextView>(R.id.tv_dialog_detail).text = message
+        rootView.findViewById<Button>(R.id.btn_dialog_yes).setOnClickListener {
+            dismiss()
+            ok_btn_callback.invoke("okay")
+        }
+        rootView.findViewById<TextView>(R.id.tv_dialog_cancel).setOnClickListener {
+            dismiss()
+        }
+        return rootView
+    }
+}
--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/livestreaming/MakkahLiveFragment.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/livestreaming/MakkahLiveFragment.kt
@@ -1,6 +1,7 @@
 package com.qibla.qiblacompass.prayertime.finddirection.presentation.views.livestreaming
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,17 +9,23 @@ import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.qibla.qiblacompass.prayertime.finddirection.R
 import com.qibla.qiblacompass.prayertime.finddirection.base.BaseFragment
+import com.qibla.qiblacompass.prayertime.finddirection.common.PopUpDialog
 import com.qibla.qiblacompass.prayertime.finddirection.common.closeCurrentScreen
 import com.qibla.qiblacompass.prayertime.finddirection.common.hideActionBar
 import com.qibla.qiblacompass.prayertime.finddirection.databinding.FragmentMakkahLiveBinding
+import java.lang.System.exit
 
 
 class MakkahLiveFragment : BaseFragment<FragmentMakkahLiveBinding>(R.layout.fragment_makkah_live) {
     lateinit var webView: WebView
+    lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity?)?.hideActionBar()
@@ -33,21 +40,54 @@ class MakkahLiveFragment : BaseFragment<FragmentMakkahLiveBinding>(R.layout.frag
         binding.toolbarMakkahLive.groupToolbarSubScreenProfile.visibility = View.VISIBLE
         binding.toolbarMakkahLive.tvToolbarSubScreen.text = getString(R.string.makkah_live)
         webView = binding.webView
+        progressBar = binding.progressBar
         binding.toolbarMakkahLive.viewSubScreen.setOnClickListener {
             findNavController().closeCurrentScreen()
         }
-        binding.webView.loadUrl("https://www.youtube.com/embed/xZtG7Bn2B5c")
+        //binding.webView.loadUrl("https://www.youtube-nocookie.com/embed/PEmRPDJ9I8M?autoplay=1&playsinline=1")
         binding.imgMakkahViewOne.setOnClickListener {
-            loadUrl("https://www.youtube.com/embed/xZtG7Bn2B5c")
+            loadUrl("https://www.youtube-nocookie.com/embed/PEmRPDJ9I8M?autoplay=1&playsinline=1")
         }
         binding.imgMakkahViewTwo.setOnClickListener {
-            loadUrl("https://www.youtube.com/embed/xZtG7Bn2B5c")
+            loadUrl("https://www.youtube-nocookie.com/embed/Cy3hsDtzfOE?autoplay=1&playsinline=1")
         }
         binding.imgMakkahViewThree.setOnClickListener {
-            loadUrl("https://youtu.be/PEmRPDJ9I8M")
+            loadUrl("https://www.youtube-nocookie.com/embed/xZtG7Bn2B5c?autoplay=1&playsinline=1")
 
         }
-        // initializeWebView()
+        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                PopUpDialog(
+                    getString(R.string.warning),
+                    getString(R.string.exit),
+                    ok_btn_callback(),
+                    R.drawable.ic_warning
+                ).show(requireActivity().supportFragmentManager, "")
+            }
+        })
+        // Set up the WebChromeClient to enable video playback and track loading progress
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                if (newProgress == 100) {
+                    // Hide the progress bar when loading is complete
+                    progressBar.visibility = View.GONE
+                } else {
+                    // Show the progress bar and update its progress
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                }
+            }
+        }
+        // Set up the WebViewClient to handle page navigation
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                // Hide the progress bar when the page is fully loaded
+                progressBar.visibility = View.GONE
+            }
+        }
+        initializeWebView()
     }
 
     private fun loadUrl(url: String) {
@@ -56,6 +96,13 @@ class MakkahLiveFragment : BaseFragment<FragmentMakkahLiveBinding>(R.layout.frag
         webView.settings.javaScriptEnabled = true
         // Add any other customizations to your WebView as needed
         webView.loadUrl(url)
+    }
+
+    fun ok_btn_callback(): (String) -> Unit {
+        return {
+            Log.d("MakkahLiveFragment"::class.simpleName, "ok_btn_callback: ")
+            findNavController().closeCurrentScreen()
+        }
     }
 
     private fun initializeWebView() {
