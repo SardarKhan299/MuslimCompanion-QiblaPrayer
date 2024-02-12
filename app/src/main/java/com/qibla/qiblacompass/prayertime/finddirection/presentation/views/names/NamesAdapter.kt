61170acd35e092839f236f8546f7e871a6c80828
amalik <amalik@stellatechnology.com>
2024-02-12 16:48:35 +0500

    Add Allah and Rasool audio list in app level file.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/names/NamesAdapter.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/names/NamesAdapter.kt
@@ -7,7 +7,6 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.qibla.qiblacompass.prayertime.finddirection.R
 import com.qibla.qiblacompass.prayertime.finddirection.presentation.views.tasbihcounter.TasbihCounterAdapter
-
 class NamesAdapter(
     private var imageList: ArrayList<NamesData>,
     private val onItemClick: (NamesData) -> Unit
@@ -39,8 +38,8 @@ class NamesAdapter(
 
     inner class NamesViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val imageViewName: ImageView = itemView.findViewById(R.id.img_names_item)
-        val imageViewNameNumber :ImageView = itemView.findViewById(R.id.img_name_number_item)
-        val mainbg :View = itemView.findViewById(R.id.view_names)
+        val imageViewNameNumber: ImageView = itemView.findViewById(R.id.img_name_number_item)
+        val mainbg: View = itemView.findViewById(R.id.view_names)
 
         init {
             mainbg.setOnClickListener {
@@ -50,5 +49,49 @@ class NamesAdapter(
         }
     }
 
-
 }
+
+//class NamesAdapter(
+//    private var imageList: ArrayList<NamesData>,
+//    private val onItemClick: (NamesData) -> Unit
+//
+//) :
+//    RecyclerView.Adapter<NamesAdapter.NamesViewHolder>() {
+//
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NamesViewHolder {
+//        return NamesViewHolder(
+//            LayoutInflater.from(parent.context)
+//                .inflate(R.layout.items_names_layout, parent, false)
+//        )
+//    }
+//
+//    override fun onBindViewHolder(holder: NamesViewHolder, position: Int) {
+//        val image = imageList[position]
+//        holder.imageViewName.setImageResource(image.nameImage)
+//        holder.imageViewNameNumber.setImageResource(image.nameNumberImage)
+//
+//    }
+//
+//    override fun getItemCount(): Int = imageList.size// Number of items
+//
+//    fun setData(newImageList: ArrayList<NamesData>) {
+//        imageList = newImageList
+//        notifyDataSetChanged()
+//    }
+//
+//    inner class NamesViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+//        val imageViewName: ImageView = itemView.findViewById(R.id.img_names_item)
+//        val imageViewNameNumber :ImageView = itemView.findViewById(R.id.img_name_number_item)
+//        val mainbg :View = itemView.findViewById(R.id.view_names)
+//
+//        init {
+//            mainbg.setOnClickListener {
+//                val position = adapterPosition
+//                onItemClick(imageList[position])
+//            }
+//        }
+//    }
+
+
+//}
