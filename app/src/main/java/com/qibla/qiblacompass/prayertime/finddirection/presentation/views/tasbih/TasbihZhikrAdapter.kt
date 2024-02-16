cca02d2b49691004e45fe91126f0b403f0ed13e1
SardarkhanStella <skhan@stellatechnology.com>
2024-02-16 20:03:36 +0500

    Fix issue of click for Tasbih layout.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/tasbih/TasbihZhikrAdapter.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/tasbih/TasbihZhikrAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.qibla.qiblacompass.prayertime.finddirection.R
 import com.qibla.qiblacompass.prayertime.finddirection.common.SharedPreferences
@@ -25,8 +26,12 @@ class TasbihZhikrAdapter(
     override fun onBindViewHolder(holder: TasbihZhikrViewHolder, position: Int) {
         val currentItem = data[position]
 
-        holder.itemView.setOnClickListener {
-            onItemClick(currentItem.tvZhikr)
+        holder.itemLayout.setOnClickListener {
+            onItemClick.invoke(currentItem.tvZhikr)
+        }
+
+        holder.btnStart.setOnClickListener {
+            onItemClick.invoke(currentItem.tvZhikr)
         }
 
         holder.bind(currentItem)
@@ -35,6 +40,8 @@ class TasbihZhikrAdapter(
     override fun getItemCount() = data.size
 
     inner class TasbihZhikrViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemLayout: ConstraintLayout = itemView.findViewById(R.id.item_view_tasbih)
+        val btnStart: Button = itemView.findViewById(R.id.btn_start)
         fun bind(item: TasbihZhikrData) {
             itemView.findViewById<ImageView>(R.id.img_zhikr).setImageResource(item.imgZhikr)
             itemView.findViewById<TextView>(R.id.tv_zhikr).text = item.tvZhikr
