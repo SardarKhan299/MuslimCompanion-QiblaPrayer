ca576b90ccfcbb9d166220f9e8714fd9ac414d38
amalik <amalik@stellatechnology.com>
2024-06-24 15:41:24 +0500

    Implemented banner ad functionality in about us screen.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/aboutus/AboutUsFragment.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/aboutus/AboutUsFragment.kt
@@ -5,18 +5,22 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.qibla.qiblacompass.prayertime.finddirection.BuildConfig
 import com.qibla.qiblacompass.prayertime.finddirection.R
 import com.qibla.qiblacompass.prayertime.finddirection.base.BaseFragment
+import com.qibla.qiblacompass.prayertime.finddirection.common.AdUtil
 import com.qibla.qiblacompass.prayertime.finddirection.common.ApplicationConstant
 import com.qibla.qiblacompass.prayertime.finddirection.common.closeCurrentScreen
 import com.qibla.qiblacompass.prayertime.finddirection.common.hideActionBar
 import com.qibla.qiblacompass.prayertime.finddirection.databinding.FragmentAboutUsBinding
+import com.qibla.qiblacompass.prayertime.finddirection.presentation.views.dashboard.DashBoardFragment
 
 
 class AboutUsFragment : BaseFragment<FragmentAboutUsBinding>(R.layout.fragment_about_us) {
 
-
+    private lateinit var adView: AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity?)?.hideActionBar()
@@ -43,6 +47,15 @@ class AboutUsFragment : BaseFragment<FragmentAboutUsBinding>(R.layout.fragment_a
         binding.viewAboutUsTwitter.setOnClickListener {
             loadTwitterUrl()
         }
+        MobileAds.initialize(mContext) {
+            Log.d(
+                DashBoardFragment::class.java.simpleName,
+                "onViewCreated: onInitializationCompleted"
+            )
+        }
+        adView = binding.adsBannerAboutUs
+        // Initialize AdUtil and load the banner ad
+        AdUtil.initialize(requireContext(), adView)
     }
 
     fun gotoPrivacy() {
@@ -71,5 +84,25 @@ class AboutUsFragment : BaseFragment<FragmentAboutUsBinding>(R.layout.fragment_a
         val bundle = Bundle()
         bundle.putString(ApplicationConstant.URL_TO_LOAD, "https://twitter.com/?lang=en")
         findNavController().navigate(R.id.webViewUrlLoadFragment, bundle)
+    }
+    override fun onResume() {
+        adView.resume()
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        adView.pause()
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onDestroy() {
+        adView.destroy()
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+    companion object{
+        private val TAG = "AboutUsFragment"
     }
 }
