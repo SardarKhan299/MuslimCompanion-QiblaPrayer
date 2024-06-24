5b67efd988ce669ddf65e884625f6ca681ee5413
amalik <amalik@stellatechnology.com>
2024-06-24 15:49:25 +0500

    Implemented banner ad functionality in zakat  screen.

--- app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/zakat/ZakatFragment.kt
+++ app/src/main/java/com/qibla/qiblacompass/prayertime/finddirection/presentation/views/zakat/ZakatFragment.kt
@@ -1,22 +1,27 @@
 package com.qibla.qiblacompass.prayertime.finddirection.presentation.views.zakat
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.qibla.qiblacompass.prayertime.finddirection.R
 import com.qibla.qiblacompass.prayertime.finddirection.base.BaseFragment
+import com.qibla.qiblacompass.prayertime.finddirection.common.AdUtil
 import com.qibla.qiblacompass.prayertime.finddirection.common.closeCurrentScreen
 import com.qibla.qiblacompass.prayertime.finddirection.common.hideActionBar
 import com.qibla.qiblacompass.prayertime.finddirection.databinding.FragmentZakatBinding
+import com.qibla.qiblacompass.prayertime.finddirection.presentation.views.dashboard.DashBoardFragment
 
 
 class ZakatFragment : BaseFragment<FragmentZakatBinding>(R.layout.fragment_zakat) {
     lateinit var recyclerView: RecyclerView
-
+    private lateinit var adView: AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,15 @@ class ZakatFragment : BaseFragment<FragmentZakatBinding>(R.layout.fragment_zakat
         binding.apply {
             zakatFragment = this@ZakatFragment
         }
+        MobileAds.initialize(mContext) {
+            Log.d(
+                DashBoardFragment::class.java.simpleName,
+                "onViewCreated: onInitializationCompleted"
+            )
+        }
+        adView = binding.adsBannerZakat
+        // Initialize AdUtil and load the banner ad
+        AdUtil.initialize(requireContext(), adView)
         binding.toolbarZakat.groupToolbarSubScreenProfile.visibility = View.VISIBLE
         binding.toolbarZakat.tvToolbarSubScreen.text = getString(R.string.zakat_calculator)
         binding.toolbarZakat.viewSubScreen.setOnClickListener {
@@ -61,5 +75,25 @@ class ZakatFragment : BaseFragment<FragmentZakatBinding>(R.layout.fragment_zakat
             findNavController().navigate(R.id.zakatCalculateFragment)
         }
         recyclerView.adapter = adapter
+    }
+    override fun onResume() {
+        adView.resume()
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        adView.pause()
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onDestroy() {
+        adView.destroy()
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+    companion object{
+        private val TAG = "ZakatFragment"
     }
 }
